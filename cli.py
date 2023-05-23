034f00e8937a1a7cd3ac0ca014d23da0850d907c
mima <temp@example.com>
2023-05-23 14:33:15 +0200

    added cli init draft

---
+++ cli.py
@@ -0,0 +1,14 @@
+class Cli():
+    def __init__(self, inputStr=''):
+        self.inputStr = inputStr
+
+    def update():
+        pass
+
+    def getInput():
+        self.inputStr = input()
+        self.update()
+
+
+
+
