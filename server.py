0b43ddc86ae3c104e4f6af9c17ea380cf67639f5
Oscar Hjelm <>
2023-05-24 11:27:51 +0200

    Stuff

--- server.py
+++ server.py
@@ -2,7 +2,9 @@
 # encoding: utf-8
 from PrintsCharming import PrintsCharming
 from flask import Flask, request
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 pc = PrintsCharming()
 
